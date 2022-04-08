526c9103d3d21b4342346573fae401d14c275a51
Pandy Knight <andylpk.247@gmail.com>
2022-04-08 15:01:43 -0400

    Added Cypress project stubs

---
+++ cypress/cypress/integration/matchup.spec.ts
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+
+describe('Cypress', () => {
+
+    it('Round 1: Element Interaction', () => {
+        
+    });
+
+    it('Round 2: Dropdowns', () => {
+        
+    });
+
+    it('Round 3: File Upload', () => {
+        
+    });
+
+    it('Round 4: iFrames', () => {
+        
+    });
+
+    it('Round 5: Waiting', () => {
+        
+    });
+
+    it('Round 6: Accept Alerts', () => {
+        
+    });
+
+    it('Round 6: Dismiss Alerts', () => {
+        
+    });
+
+    it('Round 6: Answer Prompts', () => {
+        
+    });
+
+    it('Round 7: Navigation to New Window', () => {
+        
+    });
+
+    it('Round 8: API Requests', () => {
+        
+    });
+
+    it('Round 9: Screenshots', () => {
+        
+    });
+
+    it('Round 10: Page Objects', () => {
+        
+    });
+
+});
